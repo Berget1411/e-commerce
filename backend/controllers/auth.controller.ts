61fee32e15b56904c2d88ff239f48ffda12f71f9
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-10-29 19:32:36 +0100

    setup access tokens

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -1,6 +1,52 @@
 import { Request, Response } from "express";
 import { SignUpValidationSchema, LoginValidationSchema } from "../schemas";
 import { findUserByEmail, createUser } from "../data/user";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import redis from "../lib/redis";
+
+dotenv.config();
+
+const generateTokens = (userId: string) => {
+  const accessToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign(
+    { userId },
+    process.env.JWT_REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId: string, refreshToken: string) => {
+  await redis.set(`refreshToken:${userId}`, refreshToken, {
+    ex: 60 * 60 * 24 * 7, // 7 days
+  });
+};
+
+const setCookies = (
+  res: Response,
+  accessToken: string,
+  refreshToken: string
+) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true, // prevent client side access
+    secure: process.env.NODE_ENV !== "development", // only send the cookie over HTTPS in production
+    maxAge: 15 * 60 * 1000, // 15 minutes
+    sameSite: "strict", // prevent CSRF attacks
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true, // prevent client side access
+    secure: process.env.NODE_ENV !== "development", // only send the cookie over HTTPS in production
+    maxAge: 60 * 60 * 24 * 7 * 1000, // 7 days
+    sameSite: "strict", // prevent CSRF attacks
+  });
+};
 
 export const signUp = async (req: Request, res: Response) => {
   const validatedFields = SignUpValidationSchema.safeParse(req.body);
@@ -16,11 +62,20 @@ export const signUp = async (req: Request, res: Response) => {
   }
   const user = await createUser({ name, email, password });
 
+  // authenticate user
+  const { accessToken, refreshToken } = await generateTokens(user.id);
+  await storeRefreshToken(user.id, refreshToken);
+
+  setCookies(res, accessToken, refreshToken);
+
   if (user instanceof Error) {
     return res.status(500).json({ message: "Internal server error" });
   }
 
-  res.status(201).json({ message: "User created successfully" });
+  res.status(201).json({
+    user: { id: user.id, name: user.name, email: user.email, role: user.role },
+    message: "User created successfully",
+  });
 };
 
 export const login = async (req: Request, res: Response) => {};
--- backend/data/user.ts
+++ backend/data/user.ts
@@ -13,13 +13,13 @@ export const findUserById = async (id: string) => {
   const result = await db.select().from(users).where(eq(users.id, id));
   return result[0];
 };
-
 export const createUser = async ({ name, email, password }: User) => {
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const result = await db
+    const [result] = await db
       .insert(users)
-      .values({ name, email, password: hashedPassword });
+      .values({ name, email, password: hashedPassword })
+      .returning();
     return result;
   } catch (error) {
     return error;
