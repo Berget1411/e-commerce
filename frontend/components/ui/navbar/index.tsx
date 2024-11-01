cfb61b67e27665370435d5b02fd33a0dfbb7baa6
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-11-01 17:33:44 +0100

    add drawer for search and cart

--- frontend/components/ui/navbar/index.tsx
+++ frontend/components/ui/navbar/index.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useState } from "react";
-import { Input } from "@/components/ui/input";
 import { LuSearch, LuUser, LuShoppingCart, LuHeart } from "react-icons/lu";
 import { Button } from "../button";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-
+import Drawer from "../drawer";
+import SearchInput from "./search-input";
 const tabs = [
   {
     label: "Women",
@@ -38,66 +38,76 @@ const tabs = [
 ];
 
 export default function Navbar() {
-  const [search, setSearch] = useState("");
-  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(search);
-  };
+  const [isDrawerOpen, setIsDrawerOpen] = useState<string | false>(false);
+
   return (
-    <header className="sticky top-0 z-50 bg-background">
-      <div className="flex items-center justify-between p-4">
-        <Link
-          href="/"
-          className="m-0 text-2xl font-bold transition-opacity hover:opacity-80"
-        >
-          SoleMates
-        </Link>
-        <form
-          action=""
-          onSubmit={handleSearch}
-          className="w-full max-w-xs max-sm:hidden"
-        >
-          <div className="relative">
-            <LuSearch className="absolute left-2 top-1/2 -translate-y-1/2 text-muted-foreground" />
-            <Input
-              type="text"
-              placeholder="Search"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              className="w-full pl-8 pr-4"
-              aria-label="Search"
-            />
-          </div>
-        </form>
-        <div>
-          <Button variant="ghost" size="icon" className="sm:hidden">
-            <LuSearch />
-          </Button>
-          <Button variant="ghost" size="icon">
-            <LuShoppingCart />
-          </Button>
-          <Button variant="ghost" size="icon">
-            <LuHeart />
-          </Button>
-          <Button variant="ghost" size="icon">
-            <LuUser />
-          </Button>
-        </div>
-      </div>
-      <div className="flex items-center gap-4 p-4">
-        {tabs.map((tab) => (
+    <>
+      <header className="sticky top-0 z-30 bg-background">
+        <div className="flex items-center justify-between p-4">
           <Link
-            href={tab.href}
-            key={tab.label}
-            className={cn(
-              "text-sm font-semibold transition-opacity hover:opacity-80",
-              tab.label === "Sale" && "text-red-500",
-            )}
+            href="/"
+            className="m-0 text-2xl font-bold transition-opacity hover:opacity-80"
           >
-            {tab.label}
+            SoleMates
           </Link>
-        ))}
-      </div>
-    </header>
+          <SearchInput />
+          <div>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="sm:hidden"
+              onClick={() => setIsDrawerOpen("search")}
+            >
+              <LuSearch />
+            </Button>
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => setIsDrawerOpen("cart")}
+            >
+              <LuShoppingCart />
+            </Button>
+            <Button variant="ghost" size="icon">
+              <LuHeart />
+            </Button>
+            <Button variant="ghost" size="icon">
+              <LuUser />
+            </Button>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 p-4">
+          {tabs.map((tab) => (
+            <Link
+              href={tab.href}
+              key={tab.label}
+              className={cn(
+                "text-sm font-semibold transition-opacity hover:opacity-80",
+                tab.label === "Sale" && "text-red-500",
+              )}
+            >
+              {tab.label}
+            </Link>
+          ))}
+        </div>
+      </header>
+      {isDrawerOpen && (
+        <Drawer
+          isOpen={isDrawerOpen === "search"}
+          onClose={() => setIsDrawerOpen(false)}
+        >
+          <div className="p-4">
+            <SearchInput drawer={true} />
+          </div>
+        </Drawer>
+      )}
+      {isDrawerOpen && (
+        <Drawer
+          isOpen={isDrawerOpen === "cart"}
+          onClose={() => setIsDrawerOpen(false)}
+        >
+          <div className="p-4">Cart</div>
+        </Drawer>
+      )}
+    </>
   );
 }
